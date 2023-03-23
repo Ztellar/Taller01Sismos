165db9e9a9e8f5d645ce0ca70cfb42427499b9b5
Ztellar <b.salamanca01@ufromail.cl>
2023-03-23 09:29:11 -0300

    Actividad 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,46 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) { //uso de main para testeos
+        double [][] nom = ingresarDatos();
+        for (int i = 0; i < nom.length; i++) {
+            for (int j = 0; j < nom[i].length; j++) {
+                System.out.print(nom[i][j] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("El sismo más fuerte es de: "+ buscarMayorSismo(nom));
+        System.out.println("La cantidad de sismos con magnitud 5 o más son: "+ contarSismos(nom));
     }
 
-    public static void ingresarDatos(){
+    public static double[][] ingresarDatos(){
         double[][] sismos = new double[24][7];
         for (int i = 0; i < sismos.length; i++) {
             for (int j = 0; j < sismos[i].length; j++) {
-                sismos[i][j] = (Math.random())*10;
+                sismos[i][j] = Math.floor(((Math.random())*10)*10)/10;
             }
         }
+        return sismos;
     }
-    public static void buscarMayorSismo(){
-
+    public static double buscarMayorSismo(double[][] nom){
+        double aux = 0;
+        for (int i = 0; i < nom.length; i++) {
+            for (int j = 0; j < nom[i].length; j++) {
+                if (aux < nom[i][j]){
+                    aux = nom[i][j];
+                }
+            }
+        }
+        return aux;
     }
-    public static void contarSismos(){
-
+    public static double contarSismos(double[][] nom){
+        double aux = 0;
+        for (int i = 0; i < nom.length; i++) {
+            for (int j = 0; j < nom[i].length; j++) {
+                if (nom[i][j] >= 5.0){
+                    aux++;
+                }
+            }
+        }
+        return aux;
     }
     public static void enviarSMS(){
 
